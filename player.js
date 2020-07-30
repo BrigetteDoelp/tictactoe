26f4953980804ea4326fd3149dd6d81963a1e526
Brigette Doelp <brigette.doelp@gmail.com>
2020-07-30 15:41:59 -0700

    Build frame for Player class

---
+++ player.js
@@ -0,0 +1,17 @@
+class Player {
+  constructor(id, token) {
+    this.id = id;
+    this.token = token;
+    this.wins = [];
+  }
+  saveWinsToStorage() {
+
+  }
+  retrieveWinsFromStorage() {
+
+  }
+};
+
+
+
+//🖤 ☠️
