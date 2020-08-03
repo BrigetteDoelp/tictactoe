27aa5f87e85a81ded233d24531aff8c7439e9da2
Brigette Doelp <brigette.doelp@gmail.com>
2020-08-03 16:02:36 -0700

    Update player wins

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 var gameBoard = document.querySelector('.game-board');
 var gameSpaces = document.querySelectorAll('.space');
+var p1Wins = document.querySelector('.p1wins');
+var p2Wins = document.querySelector('.p2wins');
 
 var newGame = new Game;
 
+window.onload = updateView
 gameBoard.addEventListener('click', updateSpaceClicked);
 
-function updateBoard() {
+function updateView() {
   for (var i = 0; i < newGame.board.length; i++) {
     if (typeof newGame.board[i] === 'boolean') {
       gameSpaces[i].innerHTML = newGame.board[i] ? newGame.p1.token : newGame.p2.token
     }
   }
-}
+  updatePlayerWins()
+};
 
 function updateSpaceClicked(event) {
   for (var i = 0; i < gameSpaces.length; i++) {
     if (gameSpaces[i] === event.target) {
       newGame.updateSpace(i)
-      updateBoard()
+      updateView()
     }
   }
+};
+
+function updatePlayerWins() {
+  p1Wins.innerText = `${newGame.p1.wins} wins`;
+  p2Wins.innerText = `${newGame.p2.wins} wins`;
+  //assign those to the innerText of the querySelectors
 }
 
 
